af80677da9a52dc44336e6dfa82b235dafa8b8a5
lazarus  morris <lazarusmorris@ymail.com>
2014-10-07 14:04:03 -0700

    no message

--- public_html/snake.js
+++ public_html/snake.js
@@ -1,7 +1,16 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+var snak;
+var context;
+var screenWidth;
+var screenHeight;
+
+function gameInitialize(){
+    var
+}
+function gameLoop() {
+    
+}
+function gameDraw(){
+    
+}
 
 
